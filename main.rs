5e6fa9e4c35422bf3ad980ba0a73b63ecd1fbb6d
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2022-02-11 03:46:38 +0100

    ground

--- main.rs
+++ main.rs
@@ -13,26 +13,26 @@ use owning_ref::OwningRef;
 type Array<T=f32> = OwningRef<Box<memmap::Mmap>, [T]>;
 
 #[throws] fn map<T>(field: &str, name: &str) -> Array<T> {
-    OwningRef::new(Box::new(unsafe{memmap::Mmap::map(&std::fs::File::open(format!("cache/{name}.{field}"))?)}?)).map(|data| unsafe{from_bytes(&*data)})
+    OwningRef::new(Box::new(unsafe{memmap::Mmap::map(&std::fs::File::open(format!("../.cache/las/{name}.{field}"))?)}?)).map(|data| unsafe{from_bytes(&*data)})
 }
 
 use image::{Image, bgra8};
 
 type Mesh = (Box<[vec3]>, Box<[[u16; 3]]>);
 struct View {
+	ground: Image<Array<f32>>,
+    image: [Image<Array<u8>>; 3],
+    buildings: Box<[Mesh]>,
     x: Array,
     y: Array,
     z: Array,
-
-    image: [Image<Array<u8>>; 3],
-    meshes: Box<[Mesh]>,
 
     start_position: f32,
     position: vec2,
     vertical_scroll: f32,
 }
 
-use {vector::{xy, size, vec2, xyz, vec3}, ui::{Widget, RenderContext as Target, widget::{EventContext, Event}}, vector::num::{Zero, IsZero}};
+use {vector::{xy, size, vec2, xyz, vec3}, ui::{Widget, RenderContext as Target, widget::{EventContext, Event}}, vector::num::IsZero};
 
 use std::f32::consts::PI;
 fn rotate(xy{x,y,..}: vec2, angle: f32) -> vec2 { xy{x: f32::cos(angle)*x+f32::sin(angle)*y, y: f32::cos(angle)*y-f32::sin(angle)*x} }
@@ -65,24 +65,101 @@ fn paint(&mut self, target: &mut Target, size: size) -> Result {
         }
     }*/
     target.fill(bgra8{b:0,g:0,r:0,a:0xFF});
-    let size = vec2::from(size);
     let transform = |p:vec3| {
+        let size = vec2::from(size);
         let yaw = (self.position.x-self.start_position)/size.x*2.*PI;
         let pitch = f32::clamp(0., (1.-self.position.y/size.y)*PI/2., PI/2.);
         let scale = size.x.min(size.y)*self.vertical_scroll;
         let z = p.z;
         let p = scale*rotate(p.xy(), yaw);
         let p = size/2.+xy{x: p.x, y: f32::cos(pitch)*p.y+f32::sin(pitch)*scale*z};
-        let p = xy{x: p.x, y: (size.y-1.) - p.y};
+        //let p = xy{x: p.x, y: (size.y-1.) - p.y};
         p
     };
     let O = transform(vec3{x:0., y:0., z:0.});
     let e = [vec3{x:1., y:0., z:0.}, vec3{x:0., y:1., z:0.}, vec3{x:0., y:0., z:1.}].map(|e| transform(e)-O);
-    /*use std::simd::{Simd, f32x16, u32x16};
-    let [x,y,z] = [&self.x, &self.y, &self.z].map(|array| unsafe { let ([], array, _) = array.align_to::<f32x16>() else { unreachable!() }; array});
-    let size = size.map(|&x| Simd::splat(x));
     use rayon::prelude::*;
-    (x, y, z).into_par_iter().for_each(|(x, y, z)| { unsafe {
+
+	let mut Z = Image::fill(size, -1./2.);
+    self.buildings.into_par_iter().for_each(|(vertices, triangles)| {
+        for triangle in triangles.iter() {
+            let vertices = triangle.map(|i| vertices[i as usize]);
+            let view = vertices.map(|xyz{x,y,z}| x * e[0] + y * e[1] + z * e[2] + O );
+            let [v0,v1,v2] = view;
+            use vector::cross;
+            let w = cross(v1-v0, v2-v0);
+            //println!("{v0:?} {v1:?} {v2:?} {w}");
+			if w <= 0. { continue; }
+            let MinMax{min,max} : MinMax<vec2> = vector::minmax(view.into_iter()).unwrap();
+            let min = xy{x: f32::max(0., min.x), y: f32::max(0., min.y)};
+            let max = xy{x: u32::min(max.x as u32+1, size.x), y: u32::min(max.y as u32+1, size.y)};
+            for y in min.y as u32 .. max.y {
+                for x in min.x as u32 .. max.x {
+                    let xy = xy{x,y};
+                    let p = xy.map(|&c| c as f32);
+                    let (w2,w0,w1) = (cross(v1-v0, p-v0), cross(v2-v1, p-v1), cross(v0-v2, p-v2));
+                    if w2 > 0. && w0 > 0. && w1 > 0. {
+                        let xyz{x,y,z} = (w0*vertices[0] + w1*vertices[1] + w2*vertices[2])/w;
+                        let i = (size.y-1-xy.y)*size.x+xy.x;
+                        if !(z > Z[i as usize]) { continue; }
+                        unsafe{(Z.as_ptr() as *mut f32).offset(i as isize).write(z)};
+						let [b,g,r] = &self.image;
+                        let u = ((x+1./2.)*(b.size.x as f32)) as u32;
+                        let v = ((y+1./2.)*(b.size.y as f32)) as u32;
+                        let index = v * b.stride + u;
+                        let b = b[index as usize];
+                        let g = g[index as usize];
+                        let r = r[index as usize];
+                        unsafe{(target.as_ptr() as *mut bgra8).offset(i as isize).write(bgra8{b,g,r,a:0xFF})};
+                    }
+                }
+            }
+        }
+    });
+
+    let ground = &self.ground;
+    (0..ground.size.y-1).into_par_iter().for_each(|y| {
+        for x in 0..ground.size.x-1 {
+            let map = |x,y| xyz{x: (x as f32)/(ground.size.x as f32)-1./2., y: (y as f32)/(ground.size.y as f32)-1./2., z: ground[xy{x,y}]};
+            for vertices in [[map(x,y),map(x+1,y),map(x+1,y+1)], [map(x,y),map(x+1,y+1),map(x,y+1)]] {
+                let view = vertices.map(|xyz{x,y,z}| x * e[0] + y * e[1] + z * e[2] + O );
+                let [v0,v1,v2] = view;
+                use vector::cross;
+                let w = cross(v1-v0, v2-v0);
+                //println!("{v0:?} {v1:?} {v2:?} {w}");
+                if w <= 0. { continue; }
+                let MinMax{min,max} : MinMax<vec2> = vector::minmax(view.into_iter()).unwrap();
+                let min = xy{x: f32::max(0., min.x), y: f32::max(0., min.y)};
+                let max = xy{x: u32::min(max.x as u32+1, size.x), y: u32::min(max.y as u32+1, size.y)};
+                for y in min.y as u32 .. max.y {
+                    for x in min.x as u32 .. max.x {
+                        let xy = xy{x,y};
+                        let p = xy.map(|&c| c as f32);
+                        let (w2,w0,w1) = (cross(v1-v0, p-v0), cross(v2-v1, p-v1), cross(v0-v2, p-v2));
+                        if w2 > 0. && w0 > 0. && w1 > 0. {
+                            let xyz{x,y,z} = (w0*vertices[0] + w1*vertices[1] + w2*vertices[2])/w;
+                            let i = (size.y-1-xy.y)*size.x+xy.x;
+                            if !(z > Z[i as usize]) { continue; }
+                            unsafe{(Z.as_ptr() as *mut f32).offset(i as isize).write(z)};
+                            let [b,g,r] = &self.image;
+                            let u = ((x+1./2.)*(b.size.x as f32)) as u32;
+                            let v = ((y+1./2.)*(b.size.y as f32)) as u32;
+                            let index = v * b.stride + u;
+                            let b = b[index as usize];
+                            let g = g[index as usize];
+                            let r = r[index as usize];
+                            unsafe{(target.as_ptr() as *mut bgra8).offset(i as isize).write(bgra8{b,g,r,a:0xFF})};
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    /*use std::simd::{Simd, f32x16, u32x16};
+    use std::arch::x86_64::*;
+    let [x,y,z] = [&self.x, &self.y, &self.z].map(|array| unsafe { let ([], array, _) = array.align_to::<f32x16>() else { unreachable!() }; array});
+    (x, y, z).into_par_iter().for_each(|(&x, &y, &z)| { unsafe {
     	let [b,g,r] = &self.image;
         let u : u32x16 = _mm512_cvttps_epu32(((x+Simd::splat(1./2.))*Simd::splat(b.size.x as f32)).into()).into();
         let v : u32x16 = _mm512_cvttps_epu32(((y+Simd::splat(1./2.))*Simd::splat(b.size.y as f32)).into()).into();
@@ -91,52 +168,20 @@ fn paint(&mut self, target: &mut Target, size: size) -> Result {
         let g : u32x16 = _mm512_i32gather_epi32(indices.into(), (g.as_ptr() as *const u8).offset(-1), 1).into();
         let r : u32x16 = _mm512_i32gather_epi32(indices.into(), (r.as_ptr() as *const u8).offset(-2), 1).into();
         let bgra = Simd::splat(0xFF_00_00_00) | r & Simd::splat(0x00_FF_00_00) | g & Simd::splat(0x00_00_FF_00) | b & Simd::splat(0x00_00_00_FF);
-        let e = e.map(|e| e.map(Simd::splat));
+        let e = e.map(|e| e.map(|&x| Simd::splat(x)));
         let O = O.map(|&x| Simd::splat(x));
-        let p = x * e[0] + y * e[1] + z * e[2] + O;
-        let p : xy<u32x16> = p.map(|p| _mm512_cvttps_epu32(p.into()).into());
+        let p = xy{x: x * e[0].x + y * e[1].x + z * e[2].x + O.x, y: x * e[0].y + y * e[1].y + z * e[2].y + O.y};
+        let p : xy<u32x16> = p.map(|&p| _mm512_cvttps_epu32(p.into()).into());
         //unsafe{bgra.scatter_select_unchecked(target, indices.lanes_lt(Simd::splat(target.len())), p.y * size.x + p.x)};
-        use std::arch::x86_64::*;
-        _mm512_mask_i32scatter_epi32(target.as_ptr() as *mut u8, _mm512_cmplt_epu32_mask(p.x.into(), size.x.into()) & _mm512_cmplt_epu32_mask(p.y.into(), size.y.into()), (p.y * size.x + p.x).into(), bgra.into(), 4);
+        _mm512_mask_i32scatter_epi32(target.as_ptr() as *mut u8, _mm512_cmplt_epu32_mask(p.x.into(), Simd::splat(size.x).into()) & _mm512_cmplt_epu32_mask(p.y.into(), Simd::splat(size.y).into()), ((Simd::splat(size.y-1)-p.y) * Simd::splat(size.x) + p.x).into(), bgra.into(), 4);
     }});*/
-
-    for (vertices, triangles) in &*self.meshes {
-        for triangle in triangles.iter() {
-            let vertices = triangle.map(|i| vertices[i as usize]);
-            let view = vertices.map(|xyz{x,y,z}| x * e[0] + y * e[1] + z * e[2] + O );
-            let MinMax{min,max} : MinMax<vec2> = vector::minmax(view.into_iter()).unwrap();
-            let min = xy{x: f32::max(0., min.x), y: f32::max(0., min.y)};
-            let max = xy{x: f32::min(max.x+1., size.x), y: f32::min(max.y+1., size.y)};
-            for y in min.y as u32 .. max.y as u32 {
-                for x in min.x as u32 .. max.x as u32 {
-                    let xy = xy{x,y};
-                    let p = xy.map(|&c| c as f32);
-                    use vector::cross;
-                    let [v0,v1,v2] = view;
-                    let (w2,w0,w1) = (cross(v1-v0, p-v0), cross(v2-v1, p-v1), cross(v0-v2, p-v2));
-                    if w2 > 0. && w0 > 0. && w1 > 0. {
-                        let [b,g,r] = &self.image;
-                        let w = cross(v1-v0, v2-v0);
-                        let vec2{x,y} = (w0*vertices[0].xy() + w1*vertices[1].xy() + w2*vertices[2].xy())/w;
-                        let u = ((x+1./2.)*(b.size.x as f32)) as u32;
-                        let v = ((y+1./2.)*(b.size.y as f32)) as u32;
-                        let index = v * b.stride + u;
-                        let b = b[index as usize];
-                        let g = g[index as usize];
-                        let r = r[index as usize];
-                        target[xy] = bgra8{b,g,r,a:0xFF};
-                    }
-                }
-            }
-        }
-    }
 
     Ok(())
 }
 }
 
 #[throws] fn size(name: &str) -> size {
-    let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("{name}.tif"))?)?};
+    let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("../data/{name}.tif"))?)?};
     let mut tiff = tiff::decoder::Decoder::new(std::io::Cursor::new(&*tiff))?;
     let (size_x, size_y) = tiff.dimensions()?;
     size{x: size_x, y: size_y}
@@ -144,9 +189,9 @@ fn paint(&mut self, target: &mut Target, size: size) -> Result {
 
 type Bounds = MinMax<vec3>;
 
-#[throws] fn image_bounds(name: &str) -> Bounds {
+#[throws] fn raster_bounds(name: &str) -> Bounds {
     let size = size(name)?;
-    let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("{name}.tif"))?)?};
+    let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("../data/{name}.tif"))?)?};
     let mut tiff = tiff::decoder::Decoder::new(std::io::Cursor::new(&*tiff))?;
     let [_, _, _, E, N, _] = tiff.get_tag_f64_vec(tiff::tags::Tag::ModelTiepointTag)?[..] else { panic!() };
     let [scale_E, scale_N, _] = tiff.get_tag_f64_vec(tiff::tags::Tag::ModelPixelScaleTag)?[..] else { panic!() };
@@ -156,39 +201,49 @@ type Bounds = MinMax<vec3>;
 }
 
 #[throws] fn points_bounds(name: &str) -> Bounds {
-    let reader = las::Reader::from_path(format!("{name}.las"))?;
+    let reader = las::Reader::from_path(format!("../data/{name}.las"))?;
     let las::Bounds{min, max} = las::Read::header(&reader).bounds();
     MinMax{min: vec3{x: min.x as f32, y: min.y as f32, z: min.z as f32}, max: vec3{x: max.x as f32, y: max.y as f32, z: max.z as f32}}
 }
 
 impl View {
 fn new(image: &str, points: &str) -> Result<Self> {
-    let image_bounds = image_bounds(image)?;
+    let image_bounds = raster_bounds(image)?;
     let MinMax{min, max} = image_bounds.clip(points_bounds(points)?);
     //println!("{min:?} {max:?}");
-
     let center = (1./2.)*(min+max);
     let extent = max-min;
     let extent = extent.x.min(extent.y);
 
-    /*let mut X = Vec::with_capacity(las::Read::header(&reader).number_of_points() as usize);
-    let mut Y = Vec::with_capacity(las::Read::header(&reader).number_of_points() as usize);
-    let mut Z = Vec::with_capacity(las::Read::header(&reader).number_of_points() as usize);
-    for point in las::Read::points(&mut reader) {
-        let las::Point{x: E,y: N, z, ..} = point.unwrap();
-        let x = (E as f32-center.x)/extent;
-        let y = (N as f32-center.y)/extent;
-        let z = (z as f32-center.z)/extent;
-        if x > -1./2. && x < 1./2. && y > -1./2. && y < 1./2. {
-            X.push(x);
-            Y.push(y);
-            Z.push(z);
-        }
-    }
-    #[throws] fn write<T:bytemuck::Pod>(name: &str, field: &str, points: &[T]) { std::fs::write(format!("cache/{name}.{field}"), bytemuck::cast_slice(points))? }
-    write(points, "x", &X)?;
-    write(points, "y", &Y)?;
-    write(points, "z", &Z)?;*/
+	let ground = {
+		let name = "swissalti3d_2020_2684-1248_0.5_2056_5728";
+		let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("../data/{name}.tif"))?)?};
+		let mut tiff = tiff::decoder::Decoder::new(std::io::Cursor::new(&*tiff))?;
+		let tiff::decoder::DecodingResult::F32(ground) = tiff.read_image()? else { panic!() };
+	    let size = size(name)?;
+        let stride = size.x;
+        let ground = { // Flip Y and normalizes height
+            let mut target = unsafe{Box::new_uninit_slice(ground.len()).assume_init()};
+            for y in 0..size.y {
+                for x in 0..size.x {
+                    target[(y*stride+x) as usize] = (ground[((size.y-1-y)*stride+x) as usize]-center.z)/extent;
+                }
+            }
+            target
+        };
+        std::fs::write(format!("../.cache/las/{points}.ground"), bytemuck::cast_slice(&ground))?;
+
+		let bounds = raster_bounds(name)?;
+        let scale = size.x as f32 / (bounds.max - bounds.min).x;
+        assert!(scale == 2., "{scale}");
+        let min = scale*(min-bounds.min);
+        assert_eq!(min.x, f32::trunc(min.x));
+        assert_eq!(min.y, f32::trunc(min.y));
+        let max = scale*(max-bounds.min);
+        let size = xy{x: max.x as u32 - min.x as u32, y: max.y as u32 - min.y as u32};
+        let ground = map("ground", points).unwrap();
+        Image::strided(size, ground.map(|data| &data[((min.y as u32)*stride+(min.x as u32)) as usize..][..(size.y*stride) as usize]), stride)
+    };
 
     let [r,g,b] = {
         let size = size(image)?;
@@ -199,7 +254,7 @@ fn new(image: &str, points: &str) -> Result<Self> {
         assert_eq!(min.y, f32::trunc(min.y));
         let max = scale*(max-image_bounds.min);
 
-        /*let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("{image}.tif"))?)?};
+        /*let tiff = unsafe{memmap::Mmap::map(&std::fs::File::open(format!("../data/{image}.tif"))?)?};
         let mut tiff = tiff::decoder::Decoder::new(std::io::Cursor::new(&*tiff))?.with_limits(tiff::decoder::Limits::unlimited());
         let tiff::decoder::DecodingResult::U8(mut rgba) = tiff.read_image()? else { panic!() };
         println!("OK");
@@ -227,7 +282,7 @@ fn new(image: &str, points: &str) -> Result<Self> {
             }
             flip
         };
-        std::fs::write(format!("cache/{image}.rgb"), flip)?;*/
+        std::fs::write(format!("../.cache/las/{image}.rgb"), flip)?;*/
 
         let size = xy{x: max.x as u32 - min.x as u32, y: max.y as u32 - min.y as u32};
         iter::eval(|plane| {
@@ -236,10 +291,10 @@ fn new(image: &str, points: &str) -> Result<Self> {
         })
     };
 
-    /*let meshes = dxf::Drawing::load_file("cache/SWISSBUILDINGS3D_2_0_CHLV95LN02_1091-23.dxf")?.entities();
-    std::fs::write("cache/meshes", bincode::serialize(&meshes.collect::<Vec<_>>())?)?;*/
-    let meshes : Vec<dxf::entities::Entity> = bincode::deserialize(&std::fs::read("cache/mesh")?)?;
-    let meshes = meshes.into_iter().filter_map(|mesh| if let dxf::entities::EntityType::Polyline(mesh) = mesh.specific {
+    /*let buildings = dxf::Drawing::load_file("../data/SWISSBUILDINGS3D_2_0_CHLV95LN02_1091-23.dxf")?.entities();
+    std::fs::write("../.cache/las/1091-23.buildings", bincode::serialize(&buildings.collect::<Vec<_>>())?)?;*/
+    let buildings : Vec<dxf::entities::Entity> = bincode::deserialize(&std::fs::read("../.cache/las/1091-23.buildings")?)?;
+    let buildings = buildings.into_iter().filter_map(|mesh| if let dxf::entities::EntityType::Polyline(mesh) = mesh.specific {
         let mut vertices_and_indices = mesh.__vertices_and_handles.iter().peekable();
         let mut vertices = Vec::new();
         while let Some((vertex,_)) = vertices_and_indices.next_if(|(v,_)|
@@ -260,13 +315,33 @@ fn new(image: &str, points: &str) -> Result<Self> {
         }).collect::<Box<_>>();
         Some((vertices.into_boxed_slice(),triangles))
     } else { None }).collect::<Box<_>>();
-    //println!("{} {}", meshes.iter().map(|(v,t)| v.len()*4+t.len()*3*2).sum::<usize>(), meshes.iter().map(|(_,t)| t.len()*3*4).sum::<usize>());
+    //println!("{} {}", buildings.iter().map(|(v,t)| v.len()*4+t.len()*3*2).sum::<usize>(), buildings.iter().map(|(_,t)| t.len()*3*4).sum::<usize>());
+
+    /*let mut X = Vec::with_capacity(las::Read::header(&reader).number_of_points() as usize);
+    let mut Y = Vec::with_capacity(las::Read::header(&reader).number_of_points() as usize);
+    let mut Z = Vec::with_capacity(las::Read::header(&reader).number_of_points() as usize);
+    for point in las::Read::points(&mut reader) {
+        let las::Point{x: E,y: N, z, ..} = point.unwrap();
+        let x = (E as f32-center.x)/extent;
+        let y = (N as f32-center.y)/extent;
+        let z = (z as f32-center.z)/extent;
+        if x > -1./2. && x < 1./2. && y > -1./2. && y < 1./2. {
+            X.push(x);
+            Y.push(y);
+            Z.push(z);
+        }
+    }
+    #[throws] fn write<T:bytemuck::Pod>(name: &str, field: &str, points: &[T]) { std::fs::write(format!("../.cache/las/{name}.{field}"), bytemuck::cast_slice(points))? }
+    write(points, "x", &X)?;
+    write(points, "y", &Y)?;
+    write(points, "z", &Z)?;*/
 
     let map = |field| map(field, points).unwrap();
     Ok(Self{
-        x: map("x"), y: map("y"), z: map("z"),
+    	ground,
         image: [b,g,r],
-        meshes,
+        buildings,
+        x: map("x"), y: map("y"), z: map("z"),
         start_position: 0., position: xy{x:0.,y:f32::MAX}, vertical_scroll: 1.
     })
 }
